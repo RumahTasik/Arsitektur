9b03360eb9ba4119fd4ce0fc337b7d623c32a60b
unknown <niagaredja182@gmail.com>
2025-08-29 10:53:46 +0700

    pertama

---
+++ js/script.js
@@ -0,0 +1,358 @@
+const portfolioItems = [
+  {
+    title: "Rumah 1 Lantai Classic Modern",
+    tag: ["Rumah 1 Lantai", "Minimalis Modern"],
+    img: "../img/minimalis modern/Rumah 1 Lantai.png",
+  },
+  {
+    title: "Rumah 1 Lantai Classic Modern",
+    tag: ["Rumah 1 Lantai", "Minimalis Modern"],
+    img: "../img/minimalis modern/Rumah 1 Lantai-1.png",
+  },
+  {
+    title: "Rumah 1 Lantai Classic Modern",
+    tag: ["Rumah 1 Lantai", "Minimalis Modern"],
+    img: "../img/minimalis modern/Rumah 1 Lantai-2.png",
+  },
+  {
+    title: "Rumah 1 Lantai Classic Modern",
+    tag: ["Rumah 1 Lantai", "Minimalis Modern"],
+    img: "../img/minimalis modern/Rumah 1 Lantai-3.png",
+  },
+  {
+    title: "Rumah 1 Lantai Classic Modern",
+    tag: ["Rumah 1 Lantai", "Minimalis Modern"],
+    img: "../img/minimalis modern/Rumah 1 Lantai-4.png",
+  },
+  {
+    title: "Rumah 1 Lantai Classic Modern",
+    tag: ["Rumah 1 Lantai", "Minimalis Modern"],
+    img: "../img/minimalis modern/Rumah 1 Lantai-5.png",
+  },
+  {
+    title: "Rumah 1 Lantai Model Scandinavian",
+    tag: ["Rumah 1 Lantai", "Skandinavian"],
+    img: "../img/Skandinavian/Rumah 1 Lantai-6.png",
+  },
+  {
+    title: "Rumah 1 Lantai Model Scandinavian",
+    tag: ["Rumah 1 Lantai", "Skandinavian"],
+    img: "../img/Skandinavian/Rumah 1 Lantai-7.png",
+  },
+  {
+    title: "Rumah 1 Lantai Model Scandinavian",
+    tag: ["Rumah 1 Lantai", "Skandinavian"],
+    img: "../img/Skandinavian/Rumah 1 Lantai-8.png",
+  },
+  {
+    title: "Rumah 1 Lantai Model Scandinaviann",
+    tag: ["Rumah 1 Lantai", "Skandinavian"],
+    img: "../img/Skandinavian/Rumah 1 Lantai-9.png",
+  },
+  {
+    title: "Rumah 1 Lantai Model Scandinavian",
+    tag: ["Rumah 1 Lantai", "Skandinavian"],
+    img: "../img/Skandinavian/Rumah 1 Lantai-6.png",
+  },
+  {
+    title: "Rumah 2 Lantai Model Classic",
+    tag: ["Rumah 2 Lantai", "Classic"],
+    img: "../img/classic/Rumah 2 Lantai-1.png",
+  },
+  {
+    title: "Rumah 2 Lantai Model Classic",
+    tag: ["Rumah 2 Lantai", "Classic"],
+    img: "../img/classic/Rumah 2 Lantai-2.png",
+  },
+  {
+    title: "Rumah 2 Lantai Model Classic",
+    tag: ["Rumah 2 Lantai", "Classic"],
+    img: "../img/classic/Rumah 2 Lantai-3.png",
+  },
+  {
+    title: "Rumah 2 Lantai Model Classic",
+    tag: ["Rumah 2 Lantai", "Classic"],
+    img: "../img/classic/Rumah 2 Lantai-4.png",
+  },
+  {
+    title: "Rumah 2 Lantai Model Classic",
+    tag: ["Rumah 2 Lantai", "Classic"],
+    img: "../img/classic/Rumah 2 Lantai-5.png",
+  },
+  {
+    title: "Rumah 2 Lantai Model Classic",
+    tag: ["Rumah 2 Lantai", "Classic"],
+    img: "../img/classic/Rumah 2 Lantai-6.png",
+  },
+  {
+    title: "Rumah 2 Lantai Model Classic",
+    tag: ["Rumah 2 Lantai", "Classic"],
+    img: "../img/classic/Rumah 2 Lantai-7.png",
+  },
+  {
+    title: "Rumah 2 Lantai Model Classic",
+    tag: ["Rumah 2 Lantai", "Classic"],
+    img: "../img/classic/Rumah 2 Lantai-8.png",
+  },
+  {
+    title: "Rumah 2 Lantai Model Classic",
+    tag: ["Rumah 2 Lantai", "Classic"],
+    img: "../img/classic/Rumah 2 Lantai-9.png",
+  },
+  {
+    title: "Rumah 2 Lantai Model Classic",
+    tag: ["Rumah 2 Lantai", "Classic"],
+    img: "../img/classic/Rumah 2 Lantai-10.png",
+  },
+  {
+    title: "Rumah 2 Lantai Model Classic",
+    tag: ["Rumah 2 Lantai", "Classic"],
+    img: "../img/classic/Rumah 2 Lantai-11.png",
+  },
+  {
+    title: "Rumah 2 Lantai Model Classic",
+    tag: ["Rumah 2 Lantai", "Classic"],
+    img: "../img/classic/Rumah 2 Lantai-12.png",
+  },
+  {
+    title: "Rumah 2 Lantai Model Classic",
+    tag: ["Rumah 2 Lantai", "Classic"],
+    img: "../img/classic/Rumah 2 Lantai-13.png",
+  },
+  {
+    title: "Rumah 2 Lantai Model Classic",
+    tag: ["Rumah 2 Lantai", "Classic"],
+    img: "../img/classic/Rumah 2 Lantai-14.png",
+  },
+  {
+    title: "Rumah 2 Lantai Model Content Corer",
+    tag: ["Rumah 2 Lantai", "Content Corer"],
+    img: "../img/content corer/Rumah 2 Lantai-15.png",
+  },
+  {
+    title: "Rumah 2 Lantai Model Content Corer",
+    tag: ["Rumah 2 Lantai", "Content Corer"],
+    img: "../img/content corer/Rumah 2 Lantai-16.png",
+  },
+  {
+    title: "Rumah 2 Lantai Model Content Corer",
+    tag: ["Rumah 2 Lantai", "Content Corer"],
+    img: "../img/content corer/Rumah 2 Lantai-17.png",
+  },
+  {
+    title: "Rumah 2 Lantai Model Content Corer",
+    tag: ["Rumah 2 Lantai", "Content Corer"],
+    img: "../img/content corer/Rumah 2 Lantai-18.png",
+  },
+  {
+    title: "Rumah 2 Lantai Model Content Corer",
+    tag: ["Rumah 2 Lantai", "Content Corer"],
+    img: "../img/content corer/Rumah 2 Lantai-19.png",
+  },
+  {
+    title: "Rumah 2 Lantai Model Content Corer",
+    tag: ["Rumah 2 Lantai", "Content Corer"],
+    img: "../img/content corer/Rumah 2 Lantai-20.png",
+  },
+  {
+    title: "Rumah 2 Lantai Model Content Corer",
+    tag: ["Rumah 2 Lantai", "Content Corer"],
+    img: "../img/content corer/Rumah 2 Lantai-21.png",
+  },
+  {
+    title: "Rumah 2 Lantai Model Content Corer",
+    tag: ["Rumah 2 Lantai", "Content Corer"],
+    img: "../img/content corer/Rumah 2 Lantai-22.png",
+  },
+  {
+    title: "Rumah 2 Lantai Model Content Corer",
+    tag: ["Rumah 2 Lantai", "Content Corer"],
+    img: "../img/content corer/Rumah 2 Lantai-23.png",
+  },
+  {
+    title: "Rumah 2 Lantai Model Content Corer",
+    tag: ["Rumah 2 Lantai", "Content Corer"],
+    img: "../img/content corer/Rumah 2 Lantai-24.png",
+  },
+  {
+    title: "Rumah 2 Lantai Model Content Corer",
+    tag: ["Rumah 2 Lantai", "Content Corer"],
+    img: "../img/content corer/Rumah 2 Lantai-25.png",
+  },
+  {
+    title: "Rumah 2 Lantai Model Content Corer",
+    tag: ["Rumah 2 Lantai", "Content Corer"],
+    img: "../img/content corer/Rumah 2 Lantai-26.png",
+  },
+  {
+    title: "Rumah 2 Lantai Model Content Corer",
+    tag: ["Rumah 2 Lantai", "Content Corer"],
+    img: "../img/content corer/Rumah 2 Lantai-27.png",
+  },
+  {
+    title: "Rumah 2 Lantai Model Content Corer",
+    tag: ["Rumah 2 Lantai", "Content Corer"],
+    img: "../img/content corer/Rumah 2 Lantai-28.png",
+  },
+  {
+    title: "Rumah 2 Lantai Model Content Corer",
+    tag: ["Rumah 2 Lantai", "Content Corer"],
+    img: "../img/content corer/Rumah 2 Lantai-29.png",
+  },
+  {
+    title: "Rumah 2 Lantai Model Content Corer",
+    tag: ["Rumah 2 Lantai", "Content Corer"],
+    img: "../img/content corer/Rumah 2 Lantai-30.png",
+  },
+  {
+    title: "Rumah 2 Lantai Model Content Corer",
+    tag: ["Rumah 2 Lantai", "Content Corer"],
+    img: "../img/content corer/Rumah 2 Lantai-31.png",
+  },
+  {
+    title: "Rumah 2 Lantai Model Content Corer",
+    tag: ["Rumah 2 Lantai", "Content Corer"],
+    img: "../img/content corer/Rumah 2 Lantai-32.png",
+  },
+  {
+    title: "Rumah 2 Lantai Model Content Corer",
+    tag: ["Rumah 2 Lantai", "Content Corer"],
+    img: "../img/content corer/Rumah 2 Lantai-33.png",
+  },
+  {
+    title: "Rumah 2 Lantai Model Content Corer",
+    tag: ["Rumah 2 Lantai", "Content Corer"],
+    img: "../img/content corer/Rumah 2 Lantai-34.png",
+  },
+];
+
+const grid = document.getElementById("portfolioGrid");
+const renderPortfolio = (filter = "all") => {
+  grid.innerHTML = "";
+  portfolioItems
+    .filter((item) => {
+      if (filter === "all") return true;
+
+      if (Array.isArray(item.tag)) {
+        return item.tag.includes(filter);
+      }
+
+      return item.tag === filter;
+    })
+    .forEach((item) => {
+      const div = document.createElement("div");
+      div.className = "card";
+      div.innerHTML = `
+      <a href="${item.img}" target="_blank">
+        <img src="${item.img}" alt="${item.title}"> </a>
+        <h4>${item.title}</h4>
+      `;
+      grid.appendChild(div);
+    });
+};
+renderPortfolio();
+
+document.querySelectorAll(".filter-btn").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document
+      .querySelectorAll(".filter-btn")
+      .forEach((b) => b.classList.remove("active"));
+    btn.classList.add("active");
+    renderPortfolio(btn.dataset.filter);
+  });
+});
+
+// nomor WA admin (ganti dengan nomor kamu, pakai kode negara tanpa +)
+const adminNumber = "6281381300825";
+
+// ambil semua tombol order
+const orderButtons = document.querySelectorAll(".btn-order");
+
+orderButtons.forEach((button) => {
+  button.addEventListener("click", function (e) {
+    e.preventDefault(); // supaya tidak reload halaman
+
+    const packageName = this.getAttribute("data-package");
+    const packagePrice = this.getAttribute("data-price");
+
+    const message = `Halo Min, saya ingin menggunakan Jasa Arsitek ${packageName} (${packagePrice})`;
+
+    // encode pesan biar aman untuk URL
+    const encodedMessage = encodeURIComponent(message);
+
+    // arahkan user ke WhatsApp
+    const waUrl = `https://wa.me/${adminNumber}?text=${encodedMessage}`;
+    window.open(waUrl, "_blank");
+  });
+});
+
+// menu bar
+
+const hamburger = document.getElementById("hamburger-menu");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active"); // tampil/slide-in dari kanan
+  hamburger.classList.toggle("open"); // animasi X
+});
+
+// --- Cek parameter URL untuk buka modal otomatis ---
+const params = new URLSearchParams(window.location.search);
+if (params.get("konsultasi") === "open") {
+  const modal = document.getElementById("konsultasiModal");
+  if (modal) {
+    modal.style.display = "flex";
+
+    // Hapus parameter dari URL supaya pas refresh normal lagi
+    const newUrl = window.location.origin + window.location.pathname;
+    window.history.replaceState({}, document.title, newUrl);
+  }
+}
+
+const Number = "6281381300825";
+
+// Ambil elemen
+const konsultasiBtn = document.querySelector(".kontak");
+const modal = document.getElementById("konsultasiModal");
+const closeBtn = document.querySelector(".close");
+const form = document.getElementById("konsultasiForm");
+
+// Buka modal saat klik tombol
+konsultasiBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  modal.style.display = "flex";
+});
+
+// Tutup modal saat klik close
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+// Tutup modal jika klik luar konten
+window.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
+
+// Kirim data ke WhatsApp
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const nama = document.getElementById("nama").value;
+  const wa = document.getElementById("wa").value;
+
+  // validasi Nomor Wa
+  if (!/^(?:\+62|62|0)(?:\s?\d{2,4})(?:[\s-]?\d{3,4}){2,3}$/.test(wa)) {
+    alert(
+      "Nomor WA tidak valid minimal harus 10-13 digit. Contoh:\n- 08123456789\n- +62 8123456789\n- +62 812-3456-789"
+    );
+    return;
+  }
+  const message = `Hallo Min, saya ingin konsultasi Design Arsitek dengan menggunakan Jasa Rumah Tasik.%0A
+Nama: ${nama}%0A
+WA: ${wa}%0A`;
+
+  const waUrl = `https://wa.me/${Number}?text=${message}`;
+  window.open(waUrl, "_blank");
+});
