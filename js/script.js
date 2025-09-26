fdcd4ffea4ae37c49560758caa27a29f405d572d
unknown <niagaredja182@gmail.com>
2025-09-26 10:11:57 +0700

    update

--- js/script.js
+++ js/script.js
@@ -78,6 +78,22 @@ for (let i = 1; i <= 5; i++) {
   });
 }
 
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (e) {
+  if (!e.target.matches(".dropbtn")) {
+    var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains("show")) {
+      myDropdown.classList.remove("show");
+    }
+  }
+};
+
 // ==== PORTFOLIO GRID + MODAL ====
 const portfolioGrid = document.getElementById("portfolioGrid");
 const portfolioModal = document.getElementById("portfolioModal");
